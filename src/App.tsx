c083bb00912752941c88db614e5884f249d4d2b0
tZandr <alex_tjernstrom@hotmail.com>
2026-04-15 12:24:09 +0200

    Links

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,31 @@
+import { Link } from 'react-router-dom';
+import AppRoutes from './routes';
 import './App.css';
 
 function App() {
-
   return (
     <>
-      <div id="root">
-        <div className="header">
-          <h2>React - Övning 4</h2>
-          <p>x</p>
-        </div>
-        <div className="assignment">
-        </div>
-        <div>
-          <small>Alexander Tjernström, IT-Högskolan JSU25</small>
-        </div>
+      <div className="header">
+        <h2>React - Övning 7</h2>
+        <p>Routing (old)</p>
+      </div>
+
+      <div className="assignment">
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/About">About</Link>
+          <Link to="/SongList">Songs</Link>
+        </nav>
+
+        <AppRoutes />
+      </div>
+
+      <div>
+        <small>Alexander Tjernström, IT-Högskolan JSU25</small>
       </div>
     </>
   );
 }
+
 
 export default App;
